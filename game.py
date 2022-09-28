e433bf2b38724dc107cea3d0e110d1c1a98b1176
Muuraa <114050894+Muuraa@users.noreply.github.com>
2022-09-28 09:57:50 +0300

    Now you can see history of your and computer moves

--- game.py
+++ game.py
@@ -1,14 +1,21 @@
 import random
 turns = ['rock', 'paper', 'scissors']
-human_turn = input('Enter your turn, or type exit: ')
-computer_turn = random.choice(turns)
-
+human_turns = []
+computer_turns = []
 
 while(True):
-    print(f'Human:{human_turn} vs. computer: {computer_turn}')
+    human_turn = input("Enter your turn, or type exit: ")
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        print('Thank you for playing! Bye bye')
+        break
+
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
-        if human_turn == 'exit':
-            break
         print('Draw!')
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
@@ -18,3 +25,8 @@ while(True):
         print('Human wins!')
     else:
         print('Computer wins!')
+
+
+print(f'You have played {len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
